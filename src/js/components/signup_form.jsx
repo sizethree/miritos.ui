02ab73e277c00252f10dd7aca371cbe26d42a429
Danny Hadley <dadleyy@gmail.com>
2016-08-16 22:27:44 -0400

    more work on signup form - moving sass files around slightly

--- src/js/components/signup_form.jsx
+++ src/js/components/signup_form.jsx
@@ -4,6 +4,25 @@ define([
   function render() {
     return (
       <div className="m-signup-form">
+        <div className="margin-bottom-10">
+          <h5>Sign Up</h5>
+        </div>
+        <div className="clearfix">
+          <div className="m-signup-form__input-container">
+            <input type="text" className="bordered-input" placeholder="Email" />
+          </div>
+          <div className="m-signup-form__input-container">
+            <input type="text" className="bordered-input" placeholder="Name" />
+          </div>
+          <div className="m-signup-form__input-container">
+            <input type="password" className="bordered-input" placeholder="Password" />
+          </div>
+        </div>
+        <div className="clearfix">
+          <div className="float-right">
+            <a className="rounded-button rounded-button--blue">Submit</a>
+          </div>
+        </div>
       </div>
     )
   }
