3b7960cf99ad91484389bcdb0fdb5cf75aab65a5
Danny Hadley <dadleyy@gmail.com>
2016-08-18 10:19:04 -0400

    cleaning up welcome page - removing loreum text

--- src/js/views/welcome.jsx
+++ src/js/views/welcome.jsx
@@ -7,12 +7,7 @@ define([
     return (
       <div className="clearfix padding-tb-15">
         <div className="row">
-          <div className="column large-6 text-display">
-            <h4>The art of debate - perfected.</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget velit at risus scelerisque tincidunt sit amet non tortor. In laoreet luctus dui. Curabitur finibus aliquam enim, sit amet congue magna. Phasellus in orci faucibus, convallis ipsum at, cursus mi. Pellentesque vehicula lorem nec bibendum fermentum. Vivamus nec odio non ante bibendum tempor. Maecenas in nisi commodo, eleifend lectus sed, commodo velit. Quisque eget magna imperdiet, sollicitudin arcu vel, congue dolor. Nulla vel elit sollicitudin eros auctor gravida. Quisque efficitur risus vel ligula dictum eleifend. Aliquam erat volutpat.</p>
-            <p>Ut ut mollis lectus, at ultricies ex. In quis dui porta, molestie augue in, consequat mauris. Fusce suscipit sem eu enim mollis, egestas tristique est feugiat. Pellentesque ac tincidunt libero. Aliquam ut lacus nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis elit quam, euismod eget sollicitudin a, facilisis euismod augue. Morbi cursus metus erat, in auctor nisi accumsan eu. Sed vitae ligula tincidunt, ultricies erat eu, tincidunt tellus. Vivamus sit amet pellentesque nulla, vel eleifend nulla.</p>
-          </div>
-          <div className="column large-6">
+          <div className="column large-6 large-offset-6">
             <SignUpForm />
             <LoginForm />
           </div>
