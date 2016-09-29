b6c1e65deddf8f3a3934428f3cda44fb0e30c819
Danny Hadley <dadleyy@gmail.com>
2016-09-29 01:19:02 -0400

    improving server executable by logging session errors

--- src/js/views/error.jsx
+++ src/js/views/error.jsx
@@ -3,6 +3,8 @@ export default function render() {
     <div className="row clearfix">
       <div className="columns large-6">
         <h5>error</h5>
+        <br />
+        <a className="waves-effect waves-light btn" href="/">back</a>
       </div>
     </div>
   )
