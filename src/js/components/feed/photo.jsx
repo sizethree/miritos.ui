875f1610eb9f2668ef0386163833ba1807b6e441
Danny Hadley <dadleyy@gmail.com>
2016-10-26 17:12:55 -0400

    adding images to photo feed items

--- src/js/components/feed/photo.jsx
+++ src/js/components/feed/photo.jsx
@@ -1,7 +1,16 @@
 const style = {maxWidth: "200px", maxHeight: "200px"};
 
 export default function FeedPhoto({activity, actor, object}) {
-  let {url} = object;
-  let full  = `/object?url=${encodeURIComponent(url)}`;
-  return (<div className="feed-display__feed-photo"></div>);
+  let {url, width, height} = object;
+  let full_url = `/object?url=${encodeURIComponent(url)}`;
+  let bg_style = {backgroundImage: `url(${full_url})`};
+
+  return (
+    <div className="feed-display__feed-photo">
+      <div className="feed-display__feed-photo-fs-background" style={bg_style}></div>
+      <div className="feed-display__feed-photo-container">
+        <img src={full_url} />
+      </div>
+    </div>
+  );
 }
